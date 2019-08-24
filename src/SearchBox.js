603c14eae87f6a53754b4ee47c3ff20bc6d62fd5
stealthman22 <tdotonline@gmail.com>
2019-08-24 05:06:15 +0000

    new components: all objects in robot array succesfully looped

---
+++ src/SearchBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Searchbox = () => {
+    return (
+        <div className="pa2">
+            <input className='pa3 ba b--green bg-lightest-blue'
+                type="search"
+                placeholder='Search Robots' />
+        </div>
+    );
+}
+
+export default Searchbox;
