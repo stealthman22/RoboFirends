aa75e65d1bb2f305af179c1f74b5807185c46bc9
stealthman22 <tdotonline@gmail.com>
2019-08-26 11:47:19 +0000

    Built state that connects the searchbox to the robot arrays

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
 
-const App = () => {
+
+
+
+/* const App = () => {
     return (
         <div className='tc'>
             <h1>RoboFriends</h1>
@@ -12,6 +15,40 @@ const App = () => {
         </div>
     );
 
+} */
+
+// or
+// Adding state
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    // Event function for searchbox
+    onSearchChange = (e) => {
+        // must for state to be updated.
+        this.setState({ searchfield: e.target.value })
+    }
+
+    render() {
+        // Moved from the onSearchChange method so that the render and return value can access the filterArray.
+        const filterArray = this.state.robots.filter(robot => robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()));
+        console.log(filterArray);
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList className='tc'
+                    // {changed from this.state.robots after moving filterArray.}
+                    robots={filterArray} />
+            </div>
+        );
+
+    }
 }
 
 
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Searchbox = () => {
+const Searchbox = ({ searchChange }) => {
     return (
         <div className="pa2">
             <input className='pa3 ba b--green bg-lightest-blue'
                 type="search"
-                placeholder='Search Robots' />
+                placeholder='Search Robots'
+                onChange={searchChange}
+            />
         </div>
     );
 }
