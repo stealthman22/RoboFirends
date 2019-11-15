1cd023052211f3a9967559ad8eda4a74d00c47b8
stealthman22 <tdotonline@gmail.com>
2019-11-15 23:16:05 +0000

    Built first reducer

---
+++ src/reducers.js
@@ -0,0 +1,19 @@
+import { CHANGE_SEARCH_FIELD } from './constants.js';
+
+
+// The reducers start point
+const initialState = {
+    searchField: ''
+}
+
+
+// reducer
+// Use Es6 syntax to set default parameters at once
+export const searchRobots = (state = initialState, action = {}) => {
+    switch (action.type) {
+        case: CHANGE_SEARCH_FIELD:
+            return Object.assign({}, state, searchField: action.payload);
+        default:
+            return state;
+    }
+}
