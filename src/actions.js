4c2469c70d4e3bdc1ab99003859797a4fe06ec2d
stealthman22 <tdotonline@gmail.com>
2019-11-25 10:10:17 +0000

    Fixed bug, fetch api working perfectly now

--- src/actions.js
+++ src/actions.js
@@ -20,5 +20,6 @@ export const requestRobots = () => (dispatch) => {
     fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
-        .catch(err => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: err }))
+        .catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }))
+
 }
--- src/reducers.js
+++ src/reducers.js
@@ -36,9 +36,9 @@ export const requestRobots = (state = initialStateRobots, action = {}) => {
         case REQUEST_ROBOTS_PENDING:
             return Object.assign({}, state, { isPending: true });
         case REQUEST_ROBOTS_SUCCESS:
-            return Object.assign({}.state, { robots: action.payload, isPending: false });
+            return Object.assign({}, state, { robots: action.payload, isPending: false });
         case REQUEST_ROBOTS_FAILED:
-            return Object.assign({}.state, { error: action.payload, isPending: false });
+            return Object.assign({}, state, { error: action.payload, isPending: false });
         default:
             return state;
     }
